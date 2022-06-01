09a1bdab9969507fb0c1bca8ddf3b71c4fe35e1f
andrewnnov <andrewnnov@yandex.ru>
2022-06-01 08:05:03 +0300

    operators

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-alert(1);
+//1 variables
+const firstName = "Andrey";
+//const lastName = "Ba";
+let age = 26;
+const isProgrammer = true;
+console.log(age);
+
+const with5 = "5";
+
+// 2 Мутирование
+console.log("Name: " + firstName + " а, возраст человека: " + age);
+console.log(26);
+
+//alert("Name: " + firstName + " а, возраст человека: " + age);
+
+//const lastName = prompt("Введите фамилию");
+//alert(firstName + " " + lastName);
+
+//3 Операторы
+let currentYear = 2020;
+const birthYear = 2010;
+
+age = currentYear - birthYear;
+console.log(age);
+
+const a = 10;
+const b = 5;
+let c = 32;
+
+//c = c + a;
+c += a;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(++currentYear);
+console.log(currentYear);
+
+console.log(c);
+
+//Типы данных
