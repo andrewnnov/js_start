12dfffa0ffa8a921ab3ce4151737d6149f7a4018
andrewnnov <andrewnnov@yandex.ru>
2022-06-02 19:37:46 +0300

    Number

--- app.js
+++ app.js
@@ -1,66 +1,24 @@
-//functions
+//number
+const num = 42;
+const float = 42.42;
+const pow = 10e3;
+console.log(num);
+console.log(float);
+console.log(pow);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Math.pow(2, 53) - 1);
+console.log("MAX_VALUE", Number.MAX_VALUE);
+console.log("MIN_SAFE_INTEGER", Number.MIN_SAFE_INTEGER);
+console.log("infinity", 2 / 0);
+console.log("NaN", Number.NaN); // Not a number
 
-function calculateYear(year) {
-  return 2022 - year;
-}
+const stringInt = "42";
+const stringFloat = "42.42";
+console.log(stringInt + 2);
+console.log(Number.parseInt(stringInt) + 2);
+console.log(parseInt(stringInt) + 2);
+console.log(Number(stringInt) + 2);
+console.log(+stringInt + 2);
 
-const myAge = calculateYear(1999);
-console.log(myAge);
-
-function logInfoAbout(name, year) {
-  const age = calculateYear(year);
-  if (age > 0) {
-    console.log("Человек по имени " + name + " имеет возраст " + age);
-  } else {
-    console.log("Такого не может быть");
-  }
-}
-
-logInfoAbout("Andrey", 4054);
-
-// 9 Array
-
-const cars = ["mazda", "BMW", "IZh"];
-const carsNew = new Array("mazda", "BMW", "IZh");
-console.log(cars);
-console.log(carsNew);
-console.log(cars[1]);
-console.log(cars[4]);
-console.log(cars.length);
-
-cars[0] = "Porshe";
-cars[4] = "Жигуль";
-cars[cars.length] = "Жигульlast";
-
-console.log(cars);
-
-//Циклы
-
-for (let i = 0; i < cars.length; i++) {
-  console.log(cars[i]);
-}
-
-for (let car of cars) {
-  console.log(car);
-}
-
-// 11 Objects
-const person = {
-  firstName: "Andrew",
-  lastName: "Surname",
-  year: 1993,
-  language: ["Ru", "En", "De"],
-  hasWife: false,
-  greet: function () {
-    console.log("greet from person");
-  },
-};
-
-console.log(person);
-console.log(person.firstName);
-console.log(person["lastName"]);
-person.newItem = "New Item";
-
-console.log(person);
-
-person.greet();
+console.log("?", 0.4 + 0.2);
+console.log(parseFloat((0.4 + 0.2).toFixed(1)));
